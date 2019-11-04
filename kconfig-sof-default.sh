e8e2db2fc432ee01f563853b83ecdccec2ed1cea
Pan Xiuli <xiuli.pan@linux.intel.com>
2019-11-04 13:19:30 +0800

    scripts: add attached options for kconfig
    We can attach any debug kconfig or board specific defconfig with the
    scripts.
    
    EG: ../kconfig/kconfig-sof-default.sh ../kconfig/debug-defconfig
    
    Signed-off-by: Pan Xiuli <xiuli.pan@linux.intel.com>

--- kconfig-sof-default.sh
+++ kconfig-sof-default.sh
@@ -2,4 +2,4 @@
 KCONFIG_DIR=$(dirname ${BASH_SOURCE[0]})
 echo $KCONFIG_DIR
 make defconfig
-scripts/kconfig/merge_config.sh .config $KCONFIG_DIR/base-defconfig $KCONFIG_DIR/sof-defconfig $KCONFIG_DIR/sof-mach-driver-defconfig $KCONFIG_DIR/hdaudio-codecs-defconfig $KCONFIG_DIR/soundwire-defconfig
+scripts/kconfig/merge_config.sh .config $KCONFIG_DIR/base-defconfig $KCONFIG_DIR/sof-defconfig $KCONFIG_DIR/sof-mach-driver-defconfig $KCONFIG_DIR/hdaudio-codecs-defconfig $KCONFIG_DIR/soundwire-defconfig $@
--- kconfig-sst.sh
+++ kconfig-sst.sh
@@ -2,4 +2,4 @@
 KCONFIG_DIR=$(dirname ${BASH_SOURCE[0]})
 echo $KCONFIG_DIR
 make defconfig
-scripts/kconfig/merge_config.sh .config $KCONFIG_DIR/base-defconfig $KCONFIG_DIR/sst-defconfig $KCONFIG_DIR/hdaudio-codecs-defconfig
+scripts/kconfig/merge_config.sh .config $KCONFIG_DIR/base-defconfig $KCONFIG_DIR/sst-defconfig $KCONFIG_DIR/hdaudio-codecs-defconfig $@
