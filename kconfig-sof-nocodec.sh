c6bdbe0135d1d0e3352852d52f9ddbc767123df6
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-11-16 09:04:04 -0600

    sof-dev-defconfig: move all developer options to separate fragment
    the file sof-defconfig should only contain options that can be used
    without issues by distros.
    
    The file sof-dev-defconfig contains all developer options and is still
    used for SOF CI tests.
    
    Also remove options such as HASWELL, SKYLAKE, KABYLAKE which are not supported
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- kconfig-sof-nocodec.sh
+++ kconfig-sof-nocodec.sh
@@ -2,4 +2,9 @@
 KCONFIG_DIR=$(dirname ${BASH_SOURCE[0]})
 echo $KCONFIG_DIR
 make defconfig
-scripts/kconfig/merge_config.sh .config $KCONFIG_DIR/base-defconfig $KCONFIG_DIR/sof-defconfig $KCONFIG_DIR/nocodec-defconfig $@
+scripts/kconfig/merge_config.sh .config \
+				$KCONFIG_DIR/base-defconfig    \
+				$KCONFIG_DIR/sof-defconfig     \
+				$KCONFIG_DIR/sof-dev-defconfig \
+				$KCONFIG_DIR/nocodec-defconfig \
+				$@
