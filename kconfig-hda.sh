b92a506df0e0209dbf915614adef007aad48cd02
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-11-16 09:34:38 -0600

    kconfig*-sh: find merge_config.sh in work directory
    When using the scripts with a ktest.pl environment, the .config
    is in the -build directory but the script isn't.
    
    Add a check and indirectory to use the script as is from -build directory
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- kconfig-hda.sh
+++ kconfig-hda.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 KCONFIG_DIR=$(dirname ${BASH_SOURCE[0]})
 echo $KCONFIG_DIR
+
+BUILD_DIR=$(pwd)
+
+# find merge_config in code directory
+FILE=scripts/kconfig/merge_config.sh
+if test -f "$FILE"; then
+    COMMAND=$FILE;
+else
+    # try if the script in a work directory without the -build path
+    CODE_DIR=${BUILD_DIR%-build}
+    if test -f "$CODE_DIR/$FILE"; then
+	COMMAND=$CODE_DIR/$FILE;
+    else
+	echo "error: could not find $FILE";
+	exit 1;
+    fi
+fi
+
 make defconfig
-scripts/kconfig/merge_config.sh .config $KCONFIG_DIR/base-defconfig $KCONFIG_DIR/hdaudio-codecs-defconfig $@
+$COMMAND .config \
+	 $KCONFIG_DIR/base-defconfig \
+	 $KCONFIG_DIR/hdaudio-codecs-defconfig \
+	 $@
--- kconfig-sof-nocodec.sh
+++ kconfig-sof-nocodec.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 KCONFIG_DIR=$(dirname ${BASH_SOURCE[0]})
 echo $KCONFIG_DIR
+
+BUILD_DIR=$(pwd)
+
+# find merge_config in code directory
+FILE=scripts/kconfig/merge_config.sh
+if test -f "$FILE"; then
+    COMMAND=$FILE;
+else
+    # try if the script in a work directory without the -build path
+    CODE_DIR=${BUILD_DIR%-build}
+    if test -f "$CODE_DIR/$FILE"; then
+	COMMAND=$CODE_DIR/$FILE;
+    else
+	echo "error: could not find $FILE";
+	exit 1;
+    fi
+fi
+
 make defconfig
-scripts/kconfig/merge_config.sh .config \
-				$KCONFIG_DIR/base-defconfig    \
-				$KCONFIG_DIR/sof-defconfig     \
-				$KCONFIG_DIR/sof-dev-defconfig \
-				$KCONFIG_DIR/nocodec-defconfig \
-				$@
+$COMMAND .config \
+	 $KCONFIG_DIR/base-defconfig    \
+	 $KCONFIG_DIR/sof-defconfig     \
+	 $KCONFIG_DIR/sof-dev-defconfig \
+	 $KCONFIG_DIR/nocodec-defconfig \
+	 $@
