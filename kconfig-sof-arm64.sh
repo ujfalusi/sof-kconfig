90953ddd1d8cc9f232be94bbb3c1084083c20660
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-03-19 13:57:38 -0500

    arm64 support: make sure ARCH=arm64 is used
    Somehow the ARCH=arm64 is not preserved by merge_config, fix
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- kconfig-sof-arm64.sh
+++ kconfig-sof-arm64.sh
@@ -19,9 +19,10 @@ else
     fi
 fi
 
-make defconfig
-$COMMAND .config \
+make defconfig ARCH=arm64
+$COMMAND -m .config \
 	 $KCONFIG_DIR/nobloat-imx-defconfig \
 	 $KCONFIG_DIR/sof-defconfig  \
 	 $KCONFIG_DIR/sof-dev-defconfig \
 	 $@
+make olddefconfig ARCH=arm64
