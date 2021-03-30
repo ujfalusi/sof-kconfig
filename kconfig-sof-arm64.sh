3cd83f370053b032d54eba9a0d1b3ba71b15a261
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2021-03-30 11:24:20 +0300

    kconfig*-sh: Move common code to separate file
    All script uses the same code to prepare the COMMAND which is the path to
    merge_config.sh script.
    
    Create a new kconfig-lib.sh file, move the common parts there and include
    it in the scripts.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- kconfig-sof-arm64.sh
+++ kconfig-sof-arm64.sh
@@ -2,22 +2,7 @@
 KCONFIG_DIR=$(dirname ${BASH_SOURCE[0]})
 echo $KCONFIG_DIR
 
-BUILD_DIR=$(pwd)
-
-# find merge_config in code directory
-FILE=scripts/kconfig/merge_config.sh
-if test -f "$FILE"; then
-    COMMAND=$FILE;
-else
-    # try if the script in a work directory without the -build path
-    CODE_DIR=${BUILD_DIR%-build}
-    if test -f "$CODE_DIR/$FILE"; then
-	COMMAND=$CODE_DIR/$FILE;
-    else
-	echo "error: could not find $FILE";
-	exit 1;
-    fi
-fi
+. $KCONFIG_DIR/kconfig-lib.sh
 
 make defconfig ARCH=arm64
 $COMMAND -m .config \
