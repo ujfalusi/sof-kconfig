b150970b401c8d280ab3d2c8fde1436f0022cdb1
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2022-02-04 10:00:54 -0600

    kconfig: add two scripts to update distro config to sof and sof-dev
    To enable SOF on a platform, it may be easier to start from the
    existing distro configurations.
    
    kconfig-distro-sof-update.sh adds the minimal SOF options needed
    kconfig-distro-sof-dev-update.sh: adds the SOF developer options
    
    Most users would need the first script only.
    
    To speed-up compilation time, the scripts rely on 'make
    localmodconfig', which assumes that all required modules are loaded
    beforehand.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

---
+++ kconfig-distro-sof-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+KCONFIG_DIR=$(dirname ${BASH_SOURCE[0]})
+echo $KCONFIG_DIR
+
+. $KCONFIG_DIR/kconfig-lib.sh
+
+echo "-sof" > localversion
+make olddefconfig
+make localmodconfig
+
+$COMMAND .config \
+	 $KCONFIG_DIR/sof-defconfig  \
+	 $KCONFIG_DIR/amd-defconfig \
+	 $KCONFIG_DIR/mach-driver-defconfig \
+	 $KCONFIG_DIR/hdaudio-codecs-defconfig \
+	 $KCONFIG_DIR/lock-stall-defconfig \
+	 $KCONFIG_DIR/soundwire-defconfig \
+	 $@
